483185372f09d83aaa0d25145854ce87f7df164c
Ryder Horne <ryder@ryderhorne.com>
2024-02-07 10:08:23 -0700

    Fixed header margins

--- components/header.jsx
+++ components/header.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 flex flex-wrap items-center justify-center space-x-2 space-y-2 md:justify-between py-5 px-4 bg-white border-b shadow-black shadow-[0_-8px_16px]">
+    <header className="sticky top-0 z-50 flex flex-wrap items-center justify-center space-y-2 md:space-y-0 md:space-x-2 md:justify-between py-5 px-4 bg-white border-b shadow-black shadow-[0_-8px_16px]">
       <Link className="flex flex-nowrap items-center" href="/">
         <Image className="w-12 mr-4" src="/assets/img/head-raw-large.png" alt="Sabercat Robotics logo" height={50} width={50} quality={100} sizes="3rem" />
         <h2 className="font-semibold">Sabercat Robotics</h2>
