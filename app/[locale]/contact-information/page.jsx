1c99b5f617d96c799251d72442035578d904a930
ryderhorne <ryderhornedev@gmail.com>
2024-09-17 08:16:22 -0700

    Removed those who graduated from contact info

--- app/[locale]/contact-information/page.jsx
+++ app/[locale]/contact-information/page.jsx
@@ -7,18 +7,6 @@ export default function Page() {
       <main className="px-6 min-[480px]:px-12">
         <section>
           <header className="text-neutral-900 mb-4">
-            <h2 className="text-3xl">Robotics Lead</h2>
-          </header>
-          <main className="mb-4 flex flex-col flex-wrap space-y-2">
-            <section>
-              <h3 className="text-2xl">Alex Stephenson</h3>
-              <p>Job Title: Team Lead</p>
-              <p>Email: <a href="mailto:alex.stephenson@sabercatrobotics.com" className="transition-colors duration-300 text-yellow-500 hover:text-yellow-600 focus-visible:text-yellow-600">alex.stephenson@sabercatrobotics.com</a></p>
-            </section>
-          </main>
-        </section>
-        <section>
-          <header className="text-neutral-900 mb-4">
             <h2 className="text-3xl">Head of Robot</h2>
           </header>
           <main className="mb-4 flex flex-col flex-wrap space-y-2">
@@ -40,21 +28,6 @@ export default function Page() {
               <p>Email: <a href="mailto:korbin.gugisberg@sabercatrobotics.com" className="transition-colors duration-300 text-yellow-500 hover:text-yellow-600 focus-visible:text-yellow-600">korbin.gugisberg@sabercatrobotics.com</a></p>
             </section>
             <section>
-              <h3 className="text-2xl">Layla Falcon</h3>
-              <p>Job Title: Audio Visual</p>
-              <p>Email: <a href="mailto:layla.falcon@sabercatrobotics.com" className="transition-colors duration-300 text-yellow-500 hover:text-yellow-600 focus-visible:text-yellow-600">layla.falcon@sabercatrobotics.com</a></p>
-            </section>
-            <section>
-              <h3 className="text-2xl">Delia Riley</h3>
-              <p>Job Title: Member of Business</p>
-              <p>Email: <a href="mailto:delia.riley@sabercatrobotics.com" className="transition-colors duration-300 text-yellow-500 hover:text-yellow-600 focus-visible:text-yellow-600">delia.riley@sabercatrobotics.com</a></p>
-            </section>
-            <section>
-              <h3 className="text-2xl">Leo Russo</h3>
-              <p>Job Title: Member of Business</p>
-              <p>Email: <a href="mailto:leo.russo@sabercatrobotics.com" className="transition-colors duration-300 text-yellow-500 hover:text-yellow-600 focus-visible:text-yellow-600">leo.russo@sabercatrobotics.com</a></p>
-            </section>
-            <section>
               <header className="text-neutral-900 mb-4">
                 <h2 className="text-3xl">Software</h2>
               </header>
@@ -63,18 +36,6 @@ export default function Page() {
                   <h3 className="text-2xl">Ryder Horne</h3>
                   <p>Job Title: Website Developer and Designer</p>
                   <p>Email: <a href="mailto:ryder.horne@sabercatrobotics.com" className="transition-colors duration-300 text-yellow-500 hover:text-yellow-600 focus-visible:text-yellow-600">ryder.horne@sabercatrobotics.com</a></p>
-                </section>
-              </main>
-            </section>
-            <section>
-              <header className="text-neutral-900 mb-4">
-                <h2 className="text-3xl">Floater</h2>
-              </header>
-              <main className="mb-4 flex flex-col flex-wrap space-y-2">
-                <section>
-                  <h3 className="text-2xl">Nikhil Sethi</h3>
-                  <p>Job Title: Floater</p>
-                  <p>Email: <a href="mailto:nikhil.sethi@sabercatrobotics.com" className="transition-colors duration-300 text-yellow-500 hover:text-yellow-600 focus-visible:text-yellow-600">nikhil.sethi@sabercatrobotics.com</a></p>
                 </section>
               </main>
             </section>
